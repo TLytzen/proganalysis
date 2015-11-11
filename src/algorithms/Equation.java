a2f706ed97a4ff71cc0c3605d22e56626143ce4f
Rasmus Tjalk-Bøggild <rasmus.tjalk-boeggild@outlook.com>
2015-11-11 09:18:17 +0100

    Simple Set Worklist implementation

--- src/algorithms/Equation.java
+++ src/algorithms/Equation.java
@@ -1,6 +1,8 @@
 package algorithms;
 
 
+import java.util.Set;
+
 public interface Equation<T extends CompleteLattice> {
 
     T evaluate(T lattice);
--- src/algorithms/worklists/SetWorklist.java
+++ src/algorithms/worklists/SetWorklist.java
@@ -1,4 +1,34 @@
 package algorithms.worklists;
 
-public class SetWorklist {
+import algorithms.Equation;
+import algorithms.Worklist;
+
+import java.util.HashSet;
+
+public class SetWorklist implements Worklist {
+
+    private static SetWorklist EmptySet = new SetWorklist();
+
+    private HashSet<Equation> equations = new HashSet<>();
+
+    @Override
+    public Worklist getEmpty() {
+        return EmptySet;
+    }
+
+    @Override
+    public void insert(Equation equation) {
+        this.equations.add(equation);
+    }
+
+    @Override
+    public Equation extract() {
+        if (this.equations.size() == 0){
+            return null;
+        }
+
+        Equation first = this.equations.iterator().next();
+        this.equations.remove(first);
+        return first;
+    }
 }
